75cf25ab053ec6108e323859f0ca4d00ad40db25
ken88ling <ken88ling@gmail.com>
2020-12-03 17:31:36 +0800

    typeDefs, resolvers

---
+++ index.js
@@ -0,0 +1,34 @@
+const { ApolloServer, gql } = require('apollo-server')
+const sessions = require('./data/sessions.json')
+
+const typeDefs = gql`
+  type Query {
+    sessions: [Session]
+  }
+  type Session {
+    id: ID
+    title: String
+    description: String
+    startsAt: String
+    endsAt: String
+    room: String
+    day: String
+    format: String
+    track: String
+      @deprecated(reason: "Too many sessions do not fit into a single track")
+    level: String
+  }
+`
+const resolvers = {
+  Query: {
+    sessions: () => {
+      return sessions
+    },
+  },
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`graphQL running at ${url}`)
+})
