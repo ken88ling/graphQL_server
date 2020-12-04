6ffbd9049015c23e84dbb52f76d94cb8a433ddc4
ken88ling <ken88ling@gmail.com>
2020-12-04 10:02:27 +0800

    Filtering by Many Cols

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ const SessionsAPI = require('./datasources/sessions')
 
 const typeDefs = gql`
   type Query {
-    sessions: [Session]
+    sessions(
+      id: ID
+      title: String
+      description: String
+      startsAt: String
+      endsAt: String
+      room: String
+      day: String
+      format: String
+      track: String
+      level: String
+    ): [Session]
     sessionById(id: ID): Session
   }
 
